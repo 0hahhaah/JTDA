6d2dfc1d3de9c3d0462a2851f9743c86e67aef55
hyojeong721 <ase0574@naver.com>
2022-04-21 15:56:50 +0900

    S06P31S102-136 Style: 사이드바 레이아웃

---
+++ fe/src/components/SlideBar.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components"
+
+const Test = styled.div`
+  background-color: red;
+`
+export default function SlideBar() {
+  return(
+    <Test>
+
+      {/* <Slider
+        getAriaLabel={() => 'Temperature range'}
+        value={value}
+        onChange={handleChange}
+        valueLabelDisplay="auto"
+        getAriaValueText={valuetext}
+      /> */}
+    </Test>
+
+  )
+}
