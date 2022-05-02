fd9ca704ca54c329afbf4a2432cdb40e1fd17d6a
j1h00 <guruboy@snu.ac.kr>
2022-05-02 17:38:43 +0900

    #S06P31S102-176 Feat: Edit thread-test-program

--- JavaAgent/my-thread-test/src/main/java/org/example/App.java
+++ JavaAgent/my-thread-test/src/main/java/org/example/App.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -12,31 +14,35 @@ public class App
     public static Object Lock2 = new Object();
 
     public static void main( String[] args ) throws InterruptedException {
-        // RUNNABLE
-        Thread hamburger = new Thread(new Hamburger());
-        hamburger.start();
-        // NEW && TERMINATED => ThreadMXBean 에 담겨있는지 확인
-        // NEW => after 30sec => RUNNABLE
-        // 3
-        Thread late_hamburger = new Thread(new Hamburger());
-        TimeUnit.SECONDS.sleep(5);
-        late_hamburger.start();
+        for (int i = 0; i < 5; i++) {
+            // RUNNABLE
+            Thread basicThread = new Thread(new BasicThread(), "Thread-Basic-" + i);
+            basicThread.start();
+        }
+
+        // NEW => after 10sec => RUNNABLE
+        Thread lateStartThread = new Thread(new BasicThread(), "Thread-Basic-" + 6);
+        TimeUnit.SECONDS.sleep(3);
+        lateStartThread.start();
 
         // RUNNABLE && BLOCKED (synchronized method)
-        Thread order = new Thread(new Order());
-        Thread newOrder = new Thread(new Order());
+        Thread order = new Thread(new Order(), "Thread-FirstOrder");
+        Thread newOrder = new Thread(new Order(), "Thread-SecondOrder");
+        Thread newNewOrder = new Thread(new Order(), "Thread-ThirdOrder");
 
         order.start();
         newOrder.start();
+        newNewOrder.start();
 
         // WAITING by orderEdit.join()
-        Thread editableOrder = new Thread(new EditableOrder());
+        Thread editableOrder = new Thread(new EditableOrder(), "Thread-EditWaitingOrder");
         editableOrder.start();
 
         // TIMED_WAITING
-        Thread presentFood = new Thread(new PresentFood());
-        presentFood.start();
+        Thread timedWaitingOrder = new Thread(new timedWaitingThread(), "Thread-TimedWaiting");
+        timedWaitingOrder.start();
 
+        TimeUnit.SECONDS.sleep(1);
         // Deadlock
         ThreadDemo1 T1 = new ThreadDemo1();
         ThreadDemo2 T2 = new ThreadDemo2();
@@ -47,14 +53,11 @@ public class App
     private static class ThreadDemo1 extends Thread {
         public void run() {
             synchronized (Lock1) {
-                System.out.println("Thread 1: Holding lock 1...");
 
                 try { Thread.sleep(10); }
                 catch (InterruptedException e) {}
-                System.out.println("Thread 1: Waiting for lock 2...");
 
                 synchronized (Lock2) {
-                    System.out.println("Thread 1: Holding lock 1 & 2...");
                 }
             }
         }
@@ -62,25 +65,28 @@ public class App
     private static class ThreadDemo2 extends Thread {
         public void run() {
             synchronized (Lock2) {
-                System.out.println("Thread 2: Holding lock 2...");
 
                 try { Thread.sleep(10); }
                 catch (InterruptedException e) {}
-                System.out.println("Thread 2: Waiting for lock 1...");
 
                 synchronized (Lock1) {
-                    System.out.println("Thread 2: Holding lock 1 & 2...");
                 }
             }
         }
     }
 }
 
-class Hamburger implements Runnable {
+class BasicThread implements Runnable {
 
     @Override
     public void run() {
+        keepRunning();
+    }
 
+    public static void keepRunning() {
+        while (true) {
+
+        }
     }
 }
 
@@ -93,9 +99,7 @@ class Order implements Runnable {
 
     public static synchronized void makeFood() {
         while (true) {
-            // 주문을 받고 음식을 만들고 있다는 가정 하에
-            // 만약 새로운 주문이 들어온다면 먼저 주문이 들어온 음식이 나올 때 까지
-            // 새로운 음식은 나올 수가 없다
+
         }
     }
 }
@@ -105,7 +109,7 @@ class OrderEdit implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(10000);
+            Thread.sleep(30000);
 
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -118,7 +122,7 @@ class EditableOrder implements Runnable {
 
     @Override
     public void run() {
-        Thread orderEdit = new Thread(new OrderEdit()); // 음식을 만드는 와중에 요청사항이 발생했다.
+        Thread orderEdit = new Thread(new OrderEdit(), "OrderEditing"); // 음식을 만드는 와중에 요청사항이 발생했다.
         orderEdit.start(); // 요청사항 대로 조리방법을 변경했다.
 
         try {
@@ -130,7 +134,7 @@ class EditableOrder implements Runnable {
     }
 }
 
-class PresentFood implements Runnable {
+class timedWaitingThread implements Runnable {
 
     @Override
     public void run() {
