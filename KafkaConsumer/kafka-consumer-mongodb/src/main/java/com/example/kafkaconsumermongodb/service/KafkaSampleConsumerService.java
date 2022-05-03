8201f76bd58eabfdbca62f635df6a91ec28ee01f
김종범 <rlawhd011@naver.com>
2022-05-03 15:09:50 +0900

    #S06P31S102-244 Feat: kafka consumer 구현 및 mongodb 연동

---
+++ KafkaConsumer/kafka-consumer-mongodb/src/main/java/com/example/kafkaconsumermongodb/service/KafkaSampleConsumerService.java
@@ -0,0 +1,27 @@
+package com.example.kafkaconsumermongodb.service;
+
+import com.example.kafkaconsumermongodb.threadlog.ThreadLog;
+import com.google.gson.Gson;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class KafkaSampleConsumerService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @KafkaListener(topics = "json_log", groupId = "test-log")
+    public void consume(String message) throws IOException {
+//        System.out.println("receive message : " + message);
+
+        ThreadLog threadLog = new Gson().fromJson(message, ThreadLog.class);
+        mongoTemplate.insert(threadLog);
+        System.out.println("thread insert");
+    }
+}
+
---
+++ KafkaConsumer/kafka-consumer-mongodb/src/main/java/com/example/kafkaconsumermongodb/threadlog/ThreadLog.java
@@ -0,0 +1,29 @@
+package com.example.kafkaconsumermongodb.threadlog;
+
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@Document(collection = "threaddump")
+public class ThreadLog {
+    @NotBlank
+    public String hostName;
+    @NotBlank
+    public String hostIp;
+    private String processId;
+
+    private String logTime;
+
+    private int threadCount;
+
+    private String vmInfo;
+
+    private String [] threadElements;
+
+    public ThreadDump [] threadDumps;
+}
