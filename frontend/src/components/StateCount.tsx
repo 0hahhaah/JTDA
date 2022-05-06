4adc5c55fa636dcbb819774b7fd839c73bd2e663
j1h00 <guruboy@snu.ac.kr>
2022-05-06 11:17:27 +0900

    #S06P31S102-258 Refactor: Edit StateCount component

--- frontend/src/components/StateCount.tsx
+++ frontend/src/components/StateCount.tsx
@@ -1,65 +1,101 @@
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-declare module 'styled-components'{
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+declare module "styled-components" {}
 
-}
-
-const Container= styled.div`
-    width: fit-content;
+const Container = styled.div`
+  width: fit-content;
 `;
 
 const Title = styled.p`
-    margin: 5px 0 5px 10px;
-    font-size: 20px; 
-    text-align: left;
+  margin: 5px 0 5px 10px;
+  font-size: 20px;
+  text-align: left;
 `;
 
-const Block = styled.div`
-    cursor: pointer;
-    display: inline-block;
-    margin: 10px;
-    width: 120px;
-    height: 120px;
-    background-color: lightgray;
+const StateBlock = styled.div`
+  display: flex;
+  gap: 10px;
+  background-color: rgb(254, 240, 138);
+`;
+
+const Card = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 120px;
+  height: 120px;
+  background-color: lightgray;
 `;
 
 const ThreadState = styled.p`
-    margin: 0;
-    padding: 10px;
-    font-size: 14px;
+  margin: 0;
+  padding: 10px;
+  font-size: 14px;
 `;
 
 const StateNum = styled.span`
-    font-size: 40px;
+  font-size: 40px;
 `;
 
-function StateCount(){
-    const navigate = useNavigate();
-    return(
-        <Container>
-            <Title>00월00일 00:00 Thread State</Title>
-            <Block>
-                <ThreadState>총 Thread 수</ThreadState>
-                <StateNum>53</StateNum>
-            </Block>
-            <Block onClick={()=>navigate("/detail")}>
-                <ThreadState>BLOCKED</ThreadState>
-                <StateNum>53</StateNum>
-            </Block>
-            <Block onClick={()=>navigate("/detail")}>
-                <ThreadState>RUNNABLE</ThreadState>
-                <StateNum>53</StateNum>
-            </Block>
-            <Block onClick={()=>navigate("/detail")}>
-                <ThreadState>WAITING</ThreadState>
-                <StateNum>53</StateNum>
-            </Block>
-            <Block onClick={()=>navigate("/detail")}>
-                <ThreadState>TIMED_WAITING</ThreadState>
-                <StateNum>53</StateNum>
-            </Block>
-        </Container>
-    )
+function StateCount() {
+  const navigate = useNavigate();
+
+  // 테스트 용 데이터
+  const [hostId, setHostId] = useState<string>("3");
+  const [dateTime, setDateTime] = useState<string>("2022-05-06 09:50:30");
+
+  interface threadInfo {
+    count: number;
+    threadState: threadState;
+  }
+  interface threadState {
+    [index: string]: number;
+    BLOCKED: number;
+    RUNNABLE: number;
+    WAITING: number;
+    TIMED_WAITING: number;
+  }
+
+  const dummyThread: threadInfo = {
+    count: 100,
+    threadState: {
+      BLOCKED: 20,
+      RUNNABLE: 40,
+      WAITING: 30,
+      TIMED_WAITING: 10,
+    },
+  };
+  const [threadInfo, setThreadInfo] = useState(dummyThread);
+
+  // 코드 중복 제거
+  const states = ["BLOCKED", "RUNNABLE", "WAITING", "TIME_WAITING"];
+  const paintCards: Array<JSX.Element> = states.map((state) => (
+    <Card
+      onClick={() =>
+        navigate(`/detail?state=${state}&host=${hostId}&datetime=${dateTime}`)
+      }
+    >
+      <ThreadState>{state}</ThreadState>
+      <StateNum>{threadInfo.threadState[state]}</StateNum>
+    </Card>
+  ));
+
+  return (
+    <Container>
+      <Title>
+        host {hostId} / {dateTime} Thread State
+      </Title>
+      <StateBlock>
+        <Card>
+          <ThreadState>총 Thread 수</ThreadState>
+          <StateNum>{threadInfo.count}</StateNum>
+        </Card>
+        {paintCards}
+      </StateBlock>
+    </Container>
+  );
 }
 
 export default StateCount;
