6699b0a329cd975f55609d2593ee5aefae16e81a
j1h00 <guruboy@snu.ac.kr>
2022-05-09 17:16:50 +0900

    #S06P31S102-275 Refactor: ThreadSideBar 를 SideBar 에 통합 / ThreadList 컴포넌트 생성

--- frontend/src/components/SideBar.tsx
+++ frontend/src/components/SideBar.tsx
@@ -1,34 +1,37 @@
-import { Logo } from './Logo'
-import styled from "styled-components"
-import SideBarList from "./SideBarList"
+import { Logo } from "./Logo";
+import styled from "styled-components";
+import SideBarList from "./SideBarList";
+import ThreadList from "./ThreadList";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Center = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`
-const Layout = styled(Center) `
+`;
+const Layout = styled(Center)`
   width: 15%;
   height: 100vh;
-  background-color: #F7F7F7;
+  background-color: #f7f7f7;
   position: fixed;
   top: 0;
   left: 0;
-  box-shadow: 0px 3px 3px #CDCDCD;
-`
+  box-shadow: 0px 3px 3px #cdcdcd;
+`;
 
-const Base = styled(Center) `
+const Base = styled(Center)`
   height: 100%;
-  padding: 1rem;  
-  `
+  padding: 1rem;
+`;
 
-const Search= styled.div`
+const Search = styled.div`
   position: relative;
   display: flex;
   width: 100%;
   margin-top: 50px;
   margin-bottom: 20px;
-`
+`;
 
 const SearchInput = styled.input`
   width: 90%;
@@ -38,7 +41,7 @@ const SearchInput = styled.input`
   padding-right: 40px;
   border: 0;
   color: #333333;
-  box-shadow: 0px 3px 3px #CDCDCD;
+  box-shadow: 0px 3px 3px #cdcdcd;
 `;
 
 const SearchIcon = styled.span`
@@ -50,20 +53,31 @@ const SearchIcon = styled.span`
 
 const Footer = styled.div`
   width: 100%;
-  background-color: #CDCDCD;
+  background-color: #cdcdcd;
   bottom: 0;
   padding: 5px 0;
   font-size: 1rem;
   text-align: center;
-`
-export default function SideBar () {
-  return ( 
+`;
+export default function SideBar() {
+  const { pathname } = useLocation();
+  const isMain: boolean = pathname === "/" ? true : false;
+
+  const [searchInput, setSearchInput] = useState<string>("");
+
+  const handleInputChange = (value: string): void => {
+    setSearchInput(value);
+  };
+
+  return (
     <Layout>
       <Base>
         <Logo></Logo>
         <Search>
-          <SearchInput 
-            placeholder='Host IP' />
+          <SearchInput
+            placeholder={isMain ? "Host IP" : "thread id / name"}
+            onChange={(e) => handleInputChange(e.target.value)}
+          />
           {/* <input 
             style={{width: '100%'}} 
             type="text" 
@@ -72,11 +86,9 @@ export default function SideBar () {
           <button >검색</button> */}
           <SearchIcon className="material-icons">search</SearchIcon>
         </Search>
-        <SideBarList/>
+        {isMain ? <SideBarList /> : <ThreadList searchInput={searchInput} />}
       </Base>
-      <Footer>
-        Copyright ⓒ E1I4
-      </Footer>
+      <Footer>Copyright ⓒ E1I4</Footer>
     </Layout>
-    )
+  );
 }
