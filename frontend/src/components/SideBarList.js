cfe81d26309e6dad8a5c4f8f99776227279a4f40
jiho <hahhaah@naver.com>
2022-05-10 02:25:44 +0900

    #S06P31S102-273 Feat: 호스트 체크박스 배열

--- frontend/src/components/SideBarList.js
+++ frontend/src/components/SideBarList.js
@@ -49,6 +49,12 @@ const ListTitle = styled.div`
   font-weight: 500;
 `;
 
+const InfoText = styled.p`
+  margin: 0;
+  color: #999999;
+  text-align: center;
+`;
+
 const HostData = styled(Center)`
   flex-direction: row;
   width: 100%;
@@ -93,7 +99,7 @@ export default function SidebarList() {
   const [startAt, setStartAt] = useState("2022-01-31");
   const [endAt, setEndAt] = useState("2022-12-31");
   // const [checkedList, setCheckedList] = useState<Array<any> | null>(null);
-  const [checkedList, setCheckedList] = useState(null);
+  const [checkedItems, setCheckedItems] = useState([]);
   const data = [
     "host1",
     "host2",
@@ -108,6 +114,7 @@ export default function SidebarList() {
     "host11",
     "host12",
   ];
+  const [bChecked, setChecked] = useState(false);
 
   const getHosts = async () => {
     await axios({
@@ -124,25 +131,30 @@ export default function SidebarList() {
       });
   };
 
-  // const onCheckedElement:any = useCallback(
-  //   (checked: boolean, list: any) => {
-  //     if(checked){
-  //       setCheckedList([...checkedList, list]);
-  //     } else {
-  //       setCheckedList(checkedList.filter((el) => el !== list));
-  //     }
-  //   },
-  // [checkedList]);
+  const onCheckedItemHandler = () => {
+    console.log("조회");
+    //잘못생각했음. 밑에거 아니고~
+    //버튼 누르면 api 요청 보내는거
+    // if (isChecked) {
+    //   checkedItems.add(id);
+    //   setCheckedItems(checkedItems);
+    // } else if (!isChecked && checkedItems.has(id)) {
+    //   checkedItems.delete(id);
+    //   setCheckedItems(checkedItems);
+    // }
+    // console.log("dd", checkedItems);
+  };
 
-  // const onCheckedElement = (id: string, isChecked: boolean) => {
-  //   if(isChecked){
-  //     checkedList.add(id);
-  //     setCheckedList(checkedList);
-  //   } else if (!isChecked && checkedList.has(id)) {
-  //     checkedList.delete(id);
-  //     setCheckedList(checkedList);
-  //   }
-  // }
+  const checkHandler = ({ target }) => {
+    setChecked(!bChecked);
+    onCheckedItemHandler(data.id, target.checked);
+  };
+
+  const onResetHandler = () => {
+    console.log("초기화");
+    checkedItems.map((e, i) => {});
+    //checkedItems 혹은 data의 checked를 다 false로
+  };
 
   useEffect(() => {
     getHosts();
@@ -152,18 +164,21 @@ export default function SidebarList() {
     <>
       <SelectedHost>
         <ListTitle>선택한 Host</ListTitle>
-        {/* <ul>
-        {
-          checkedList.map((host, i)=>{
-            return(
-              <li>
-                {i}
-              </li>
-            )
-          })
-        }
-        </ul> */}
-        <ListBox></ListBox>
+        <ListBox>
+          {checkedItems.length === 0 ? (
+            <InfoText>
+              조회할 host를
+              <br />
+              선택해주세요
+            </InfoText>
+          ) : (
+            <ul>
+              {checkedItems.map((e, i) => {
+                return <li key={i}>{e}</li>;
+              })}
+            </ul>
+          )}
+        </ListBox>
       </SelectedHost>
 
       <HostList>
@@ -173,7 +188,19 @@ export default function SidebarList() {
             return (
               <HostData>
                 <HostLabel>
-                  <HostCheckBox type="checkbox" name="host" value={host} />
+                  <HostCheckBox
+                    type="checkbox"
+                    name="host"
+                    value={host}
+                    key={i}
+                    checked={bChecked}
+                    onChange={(e) => {
+                      setChecked(e.target.value.checked);
+                      setCheckedItems([...checkedItems, e.target.value]);
+                      console.log(checkedItems);
+                      //여기까진 됐고 이제 체크해제하면 지워져야됨,
+                    }}
+                  />
                   {host}
                 </HostLabel>
               </HostData>
@@ -182,8 +209,8 @@ export default function SidebarList() {
         </ListBox>
       </HostList>
       <SelectButtons>
-        <AllCancleBtn>초기화</AllCancleBtn>
-        <ConfirmBtn>확인</ConfirmBtn>
+        <AllCancleBtn onClick={onResetHandler}>초기화</AllCancleBtn>
+        <ConfirmBtn onClick={onCheckedItemHandler}>조회</ConfirmBtn>
       </SelectButtons>
     </>
   );
