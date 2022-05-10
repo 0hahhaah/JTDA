e6aca7b8fc65758e3d9e9c37449ed3785fa09bbc
전현민 <ssiluk12345@gmail.com>
2022-05-10 12:24:38 +0900

    #S06P31S102-239 Refac: Host API 통합
    기존 Host 리스트 조회 API & Host 검색 API를 하나의 통합 검색 API로 통합
    Controller, Service 로직 분리
    DTO, Entity(domain) 혼용 분리

--- backend/src/test/java/com/ssafy/backend/api/service/HostServiceTest.java
+++ backend/src/test/java/com/ssafy/backend/api/service/HostServiceTest.java
@@ -1,12 +1,12 @@
 package com.ssafy.backend.api.service;
 
-import com.ssafy.backend.core.domain.HostList;
+import com.ssafy.backend.api.dto.response.HostListRes;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
-import java.util.List;
+//import java.time.LocalDateTime;
+//import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -16,26 +16,23 @@ public class HostServiceTest {
     @Autowired
     HostServiceImpl hostService;
 
-    @Test
-    public void HostList_GET() {
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime startDate = now.minusDays(2);
-
-        List<HostList> hostList = hostService.getHostList(startDate.toString(), now.toString());
-
-        for(HostList host : hostList) {
-            assertThat(host.getHostIp()).isNotEmpty();
-            assertThat(host.getHostName()).isNotEmpty();
-        }
-    }
+//    @Test
+//    public void HostList_GET() {
+//        LocalDateTime now = LocalDateTime.now();
+//        LocalDateTime startDate = now.minusDays(2);
+//
+//        List<HostList> hostList = hostService.getHostList(startDate.toString(), now.toString());
+//
+//        for(HostList host : hostList) {
+//            assertThat(host.getHostIp()).isNotEmpty();
+//            assertThat(host.getHostName()).isNotEmpty();
+//        }
+//    }
 
     @Test
     public void HostSearch_GET() {
-        List<HostList> hostList = hostService.getHostSearch("DESKTOP");
+        HostListRes hostListRes = hostService.getHostSearch("", "", "");
 
-        for(HostList host : hostList) {
-            assertThat(host.getHostIp()).isNotEmpty();
-            assertThat(host.getHostName()).isNotEmpty();
-        }
+        assertThat(hostListRes.getHosts()).isNotEmpty();
     }
 }
