33b9a0f80fc397f8297a63f1687c20522c5c017b
j1h00 <guruboy@snu.ac.kr>
2022-05-11 16:48:09 +0900

    #S06P31S102-277 Feat: Edit Dockerfile & shell script

--- JavaAgent/docker-thread-dump-producer/run-containers.sh
+++ JavaAgent/docker-thread-dump-producer/run-containers.sh
@@ -1,5 +1,7 @@
+tagArray=("Monitoring", "WebService", "Frontend", "Backend", "DataBase", "Monitoring", "WebService", "Frontend", "Backend", "DataBase")
+
 for i in {1..10}
 do
     echo "Running container $i"
-    docker run -d --name "thread-producer-$i" thread-producer:latest
+    docker run -d --name "thread-producer-$i" -e  CLUSTER=CLUSTER-ONE -e HOST=HOST-$i -e TAGS=${tagArray[i]} thread-producer
 done
