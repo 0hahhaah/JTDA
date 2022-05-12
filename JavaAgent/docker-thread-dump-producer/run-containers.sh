62102a7b7d983a08fc7400478228bac7dd6f35e9
j1h00 <guruboy@snu.ac.kr>
2022-05-12 16:32:03 +0900

    #S06P31S102-299 Feat: Edit interval / hostDNS name

--- JavaAgent/docker-thread-dump-producer/run-containers.sh
+++ JavaAgent/docker-thread-dump-producer/run-containers.sh
@@ -3,5 +3,6 @@ tagArray=("Monitoring", "WebService", "Frontend", "Backend", "DataBase", "Monito
 for i in {1..10}
 do
     echo "Running container $i"
-    docker run -d --name "thread-producer-$i" -e  CLUSTER=CLUSTER-ONE -e HOST=HOST-$i -e TAGS=${tagArray[i]} thread-producer
+    host="k6s1021${i}.p.ssafy.io"
+    docker run -d --name "thread-producer-$i" -e  CLUSTER=CLUSTER-ONE -e HOST=$host -e TAGS=${tagArray[i]} thread-producer
 done
