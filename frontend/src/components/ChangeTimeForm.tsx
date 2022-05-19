d29be12297542a2d544a694849c1cf9f3f8abf22
hyojeong721 <ase0574@naver.com>
2022-05-20 04:36:32 +0900

    S06P31S102-332 fix: 날짜형식

--- frontend/src/components/ChangeTimeForm.tsx
+++ frontend/src/components/ChangeTimeForm.tsx
@@ -1,5 +1,5 @@
 export default function changeTime(value: Date | null): string {
-  if (value !== null) {
+  if (value !== null && String(value).length === 42) {
     const koreaTime = new Date(
       value.getTime() - value.getTimezoneOffset() * 60000
     )
